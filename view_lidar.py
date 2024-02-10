c156368a8f125430a8295145a94649e4b6af4cbb
William F Basener <wb8by@virginia.edu>
2024-02-10 13:33:35 -0500

    minor changes

--- view_lidar.py
+++ view_lidar.py
@@ -4,7 +4,8 @@ import pyvista as pv
 import laspy
 
 #las = laspy.read('./points_lrg.las')
-las = laspy.read('./points_fray_farm.laz')
+#las = laspy.read('./points_fray_farm.laz')
+las = laspy.read('./USGS_LPC_VA_ChesapeakeBay_2015_LAS_S13_4993_30.laz')
 
 points = np.asarray([las.x,las.y,las.z]).T
 print(points.shape)
